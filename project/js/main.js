65e2de55025ff74ecfe3d4b8a020479db147da0f
zoebachman <zoe.bachman@gmail.com>
2019-09-04 16:21:58 -0400

    added login and database

--- project/js/main.js
+++ project/js/main.js
@@ -27,6 +27,49 @@ function searchWeather(){
   getWeather(searchQuery);
 }
 
+function handleSignIn(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user.email);
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+}
+
+function addMessage(postTitle, postBody){
+  var postData = {
+    title: postTitle,
+    body: postBody
+  }
+
+  // Get a reference to the database service
+  var database = firebase.database().ref("posts");
+
+  // Create a new post reference with an auto-generated id
+  var newPostRef = database.push();
+  newPostRef.set(postData);
+}
+
+function handleMessageFormSubmit(){
+  var postTitle = $("#post-title").val();
+  var postBody = $("#post-body").val();
+  console.log(postTitle);
+
+  addMessage(postTitle, postBody);
+}
+
 
 // function myFunction() {
 //   // use jQuery ($ is shorthand) to find the div on the page and then change the html
