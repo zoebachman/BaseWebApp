af5a2ff10aab6f98b08c7235e61310a08cefea5f
zoebachman <zoe.bachman@gmail.com>
2019-09-04 11:28:14 -0400

    added api key and call

--- project/js/main.js
+++ project/js/main.js
@@ -1,11 +1,23 @@
+$(document).ready(function(){
+  getWeather();
+})
 
-function myFunction() {
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // jQuery can do a lot of crazy stuff so make sure to google around to find out more
+function getWeather(){
+  var url = "https://api.openweathermap.org/data/2.5/weather?q={New York}&APPID="+apiKey;
 
-  $("#demo").html("NEWWW PARAGRAPH #javascript #fire");
-
-  // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
+  $.ajax(url, {success: function(data){
+    console.log(data); //show result of response in console
+  }})
 }
+
+
+// function myFunction() {
+//   // use jQuery ($ is shorthand) to find the div on the page and then change the html
+//   // jQuery can do a lot of crazy stuff so make sure to google around to find out more
+
+//   $("#demo").html("NEWWW PARAGRAPH #javascript #fire");
+
+//   // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
+//   $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
+// }
 
